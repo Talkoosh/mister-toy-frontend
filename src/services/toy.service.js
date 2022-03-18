bda32e5a8a2f85f6c132bff47453d58cd8226ca8
Tal Koosh <talkoosh@Tals-MacBook-Air.local>
2022-03-18 10:55:16 +0200

    converted frontend to async await

--- src/services/toy.service.js
+++ src/services/toy.service.js
@@ -14,40 +14,67 @@ const labels = ["On wheels", "Box game", "Art", "Baby", "Doll", "Puzzle", "Outdo
 
 const TOY_KEY = 'TOY';
 const API = (process.env.NODE_ENV !== 'development')
- ? '/api/toy/'
- : 'http://localhost:3030/api/toy/';
+    ? '/api/toy/'
+    : 'http://localhost:3030/api/toy/';
 
-function loadToys() {
+async function loadToys() {
     // return storageService.query(TOY_KEY)
     //     .then(toys => {
     //         if (!toys || !toys.length) toys = _createToys();
     //         return toys;
     //     })
 
-    return axios.get(API)
-        .then(res => res.data)
+    // return axios.get(API)
+    //     .then(res => res.data)
+    try {
+        const res = await axios.get(API);
+        return res.data;
+    } catch (err) {
+        console.log('Couldn\'t get toys:', err);
+    }
 }
 
-function getToyById(id) {
+async function getToyById(id) {
     // return storageService.get(TOY_KEY, id)
     //     .then(toy => {
     //         toy = { ...toy };
     //         toy.reviews = ['Bad toy', 'DONT PLAY'];
     //         return toy
     //     })
-    return axios.get(API + id)
-            .then(res => res.data)
+
+    // return axios.get(API + id)
+    //     .then(res => res.data)
+
+    try {
+        const res = await axios.get(API + id);
+        return res.data;
+    } catch (err) {
+        console.log('Couldn\'t get toy:', err);
+    }
+
 }
 
-function save(toy) {
+async function save(toy) {
     // const prm = (toy._id) ? storageService.put(TOY_KEY, toy) : storageService.post(TOY_KEY, toy);
-    const prm = (toy._id) ? axios.put(API, toy) : axios.post(API, toy)
-    return prm.then(res => res.data)
+    try {
+        const res = (toy._id) ? await axios.put(API, toy) : await axios.post(API, toy)
+        return res.data
+    } catch(err){
+        console.log('Couldn\'t save toy:', err);
+    }
 }
 
-function removeToy(toyId) {
+async function removeToy(toyId) {
     // return storageService.remove(TOY_KEY, toyId)
-    return axios.delete(API + toyId);
+
+    // return axios.delete(API + toyId);
+
+    try {
+        const res = await axios.delete(API + toyId);
+        return res.data;
+    } catch(err){
+        console.log('Couldn\'t get toys:', err);
+    }
 }
 
 
